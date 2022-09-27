2ca12b1a29d1deac492b7740ddba3e1924d92783
reky <me@reky.dev>
2022-09-27 23:27:55 +0700

    Add screenshot of `struktur percabangan`

---
+++ struktur_percabangan.py
@@ -0,0 +1,15 @@
+# input umur
+umur = int(input("Masukkan umur: "))
+
+# input minimal umur
+minimal_umur = 18
+
+# cek apakah umur lebih besar atau sama dengan minimal umur
+if umur >= minimal_umur:
+
+    # jika ya, maka tampilkan pesan
+    print("Anda sudah cukup umur untuk membuat SIM")
+
+# jika tidak, maka tampilkan pesan
+else:
+    print("Anda belum cukup umur untuk membuat SIM")
